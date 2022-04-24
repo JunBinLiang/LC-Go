40fa25147797d47a33ff00c324e5a8f5f80beb53
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-24 12:21:37 -0400

    Time: 0 ms (100.00%), Space: 1.9 MB (7.14%) - LeetHub

---
+++ 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.go
@@ -0,0 +1,13 @@
+func max(a int, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func findLUSlength(a string, b string) int {
+    if a == b {
+        return -1
+    }
+    return max(len(a), len(b))
+}
